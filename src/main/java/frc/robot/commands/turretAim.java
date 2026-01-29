329be7165cf207575f938c9c1827eaab64aba7fa
THEHACKERISFIRST <129695526+THEHACKERISFIRST@users.noreply.github.com>
2026-01-29 17:47:34 -0600

    Update turretAim.java

--- src/main/java/frc/robot/commands/turretAim.java
+++ src/main/java/frc/robot/commands/turretAim.java
@@ -2,18 +2,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Vision;
 
-public class turretAim  extends Command {
+public class TurretAim  extends Command {
   @SuppressWarnings("PMD.UnusedPrivateField")
-  private final ExampleSubsystem m_subsystem;
+  private final TurretAim m_subsystem;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(Vision TurretVision, ) {
-    m_Vision = turretVision;
+  public TurretAim(Vision TurretVision) {
+    m_Vision = TurretVision;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
